8ad4f3b997bf7c10d7dbb403a0689bd8b8db4d29
Phillip Pearson <pp@myelin.co.nz>
2021-01-05 23:03:44 -0800

    tools: Reformat board.py and change port variable to POSTBOX_PORT

--- tools/board.py
+++ tools/board.py
@@ -1,4 +1,3 @@
-from __future__ import print_function
 # Copyright 2017 Google Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -16,25 +15,46 @@ from __future__ import print_function
 import glob
 import os
 import serial
+import sys
+import time
+
+assert sys.version_info[0] >= 3, "Python 3+ required"
+
 
 def guess_port():
-    port = os.environ.get('ROM_PORT')
+    # One day we'll have a proper USB VID:PID and be able to tell the
+    # difference between a POST box and a random USB serial device, but for
+    # now...
+    port = os.environ.get("POSTBOX_PORT")
     if port:
         return port
-    for pattern in "/dev/ttyACM? /dev/ttyUSB? /dev/tty.usbserial* /dev/tty.usbmodem* /dev/tty.wchusbserial*".split():
+    for pattern in [
+        "/dev/ttyACM?",
+        "/dev/ttyUSB?",
+        "/dev/tty.usbserial*",
+        "/dev/tty.usbmodem*",
+        "/dev/tty.wchusbserial*",
+    ]:
         matches = glob.glob(pattern)
         if matches:
             return matches[0]
+
 
 class Port:
     def __init__(self):
         port = guess_port()
         if not port:
-            raise Exception("Could not guess serial port")
+            raise Exception(
+                "Could not guess serial port: please set the POSTBOX_PORT environment variable and try again."
+            )
 
         print("Opening port %s" % port)
+        start = time.time()
         self.ser = serial.Serial(port, timeout=0)
-        print("Serial port opened: %s" % repr(self.ser))
+        print("Port open; took %.2f s: %s" % (
+            time.time() - start,
+            repr(self.ser),
+        ))
 
     def __enter__(self):
         return self.ser
